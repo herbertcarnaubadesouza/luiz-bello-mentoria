5402caa168e23e92388b23bf7f8ba86052f1f12a
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2025-08-05 17:59:12 -0300

    progress bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,35 @@
-import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import './globals.css'
+import { GeistMono } from 'geist/font/mono';
+import { GeistSans } from 'geist/font/sans';
+import type { Metadata } from 'next';
+import Loader from '../components/progress'; // 👈 import novo
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+    title: 'v0 App',
+    description: 'Created with v0',
+    generator: 'v0.dev',
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <head>
-        <style>{`
+    return (
+        <html lang="en">
+            <head>
+                <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
   --font-sans: ${GeistSans.variable};
   --font-mono: ${GeistMono.variable};
 }
         `}</style>
-      </head>
-      <body>{children}</body>
-    </html>
-  )
+            </head>
+            <body>
+                <Loader />
+                {children}
+            </body>
+        </html>
+    );
 }
